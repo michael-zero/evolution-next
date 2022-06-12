bd5c8693b2f85f72c4c766c42190c18dca0bacfd
Michael <mlmbufpi@gmail.com>
2022-06-12 12:54:42 -0300

    remove

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,8 @@ export default function Home() {
       </Link>
     </div>
     <div>
-      <Viewer fileUrl={"https://lifeplace-app.s3.sa-east-1.amazonaws.com/generic/96d3e4ae-d9c3-4ee6-a067-ce291001838f.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20220610T123928Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3599&X-Amz-Credential=AKIAUB7NMU6MV7DOROAH%2F20220610%2Fsa-east-1%2Fs3%2Faws4_request&X-Amz-Signature=58b8c74614e9d8a73f6e945c4b23b0d673ef67c6d586dacdc55c48085bf8e04d"}
+      <Viewer
+       fileUrl={""}
             plugins={[defaultLayoutPluginInstance]}></Viewer>
     </div>
     </>
